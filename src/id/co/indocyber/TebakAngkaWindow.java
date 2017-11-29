0fcd38fb5cf8e3637831c59b1ac36442a7619abb
Jodi <jodinoordiansyah@gmail.com>
2017-11-29 14:33:38 +0700

    commit kedua Tebak Angka

--- src/id/co/indocyber/TebakAngkaWindow.java
+++ src/id/co/indocyber/TebakAngkaWindow.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package id.co.indocyber;
+package id.co.indocyber; 
 
 import javax.swing.JOptionPane;
 
